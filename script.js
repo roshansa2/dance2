e1c00cb0df701c2061ed968dd1e316905c37c3bf
roshansa2 <sagopalji@gmail.com>
2024-08-13 22:08:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Mobile Menu Toggle
+const mobileMenu = document.getElementById('mobileMenu');
+const navMenu = document.getElementById('navMenu');
+
+mobileMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
 
